0cdb08f356b38cdf0d248eea0c66e431d077b8de
anniechakraborty <the.demented.genius29@gmail.com>
2024-02-03 01:29:52 +0530

    back to basics

--- App.js
+++ App.js
@@ -2,39 +2,21 @@ import React from "react";
 import { 
   View,
   Text,
-  StyleSheet,
-  Image
+  StyleSheet
 } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <Image 
-        source={{
-          // uri: 'https://randomuser.me/api/portaits/men/1.jpg'
-          uri : 'https://picsum.photos/100?random=1'
-        }}
-        style={styles.image}
-      />
+      <Text>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   },
-  text : {
-    fontSize: 40
-  },
-  image : {
-    width: 100,
-    height: 100,
-    borderRadius: 100/2
-  }
 });
 
 export default App;
